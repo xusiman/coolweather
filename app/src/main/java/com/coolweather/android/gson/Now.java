f315d881d5aac85a535f19d4af2ac8e61bd5aa93
xusiman <1510789309@qq.com>
2017-10-24 08:09:59 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,20 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/10/23.
+ */
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+    public class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Weather.java
@@ -0,0 +1,27 @@
+package com.coolweather.android.gson;
+
+import android.widget.ArrayAdapter;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/10/23.
+ */
+
+public class Weather {
+
+    public String status;
+
+    public Basic basic;
+
+    public AQI aqi;
+
+    public Now now;
+
+    public Suggestion suggestion;
+
+    @SerializedName("daily_forecast")
+    public List<Forecast> forecastList;
+}
